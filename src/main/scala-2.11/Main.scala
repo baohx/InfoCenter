6fc0d350b8f0070415c1bbf6a547871705e77d3a
ba0hx <13671505207@163.com>
2015-09-10 17:16:02 +0800

    change send message

--- src/main/scala-2.11/Main.scala
+++ src/main/scala-2.11/Main.scala
@@ -12,11 +12,11 @@ class Client extends Actor {
   //val fileName = "D:\\ScalaProject\\InfoCenter\\src\\main\\scala-2.11\\NewsTheme.txt"
   val companyNames = Source.fromFile(fileName, "utf-8").getLines.toList.map(_.replace("\n", ""))
   def receive = {
-    case "Start1" =>
+    case "StartBaidu" =>
       for (companyName <- companyNames) {
         context.actorOf(Receptionist.props(companyName, s"http://news.baidu.com/ns?word=title:${companyName}&tn=newsrss&sr=0&cl=2&rn=20&ct=0")) ! "getrss"
       }
-    case "Start2" =>
+    case "StartWeixin" =>
       for (companyName <- companyNames) {
         context.actorOf(Receptionist.props(companyName, s"http://weixin.sogou.com/weixin?query=${URLEncoder.encode(companyName,"UTF-8")}&type=2&page=1")) ! "gethtml"
       }
@@ -26,8 +26,8 @@ class Client extends Actor {
 object Main extends App {
   val system = ActorSystem("InfoCenter")
   val ic = system.actorOf(Props[Client], "ic")
-  ic ! "Start1"
-  ic ! "Start2"
+  ic ! "StartBaidu"
+  ic ! "StartWeixin"
   Thread.sleep(10000)
   ic ! PoisonPill
   Thread.sleep(10000)
